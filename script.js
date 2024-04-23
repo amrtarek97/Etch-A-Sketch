4b99957dc00964bf62265ed7dc628b6710b8aa0f
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-04-23 16:56:02 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let container = document.querySelector('.sketch-container');
+// let boxDiv;
 
+function changeBackgroundColor(){
+    this.style.backgroundColor= "black";
+}
 function createSquares(){
-    for (let i = 0; i < 16; i++) {
+    
+    for (let i = 0; i < 256; i++) {
         let boxDiv = document.createElement('div');
         boxDiv.classList.add("box");
+        container.appendChild(boxDiv);
+
+        boxDiv.addEventListener('mouseover', changeBackgroundColor());
     }
+    
 }
+
+createSquares();
+
+let btn = document.querySelector('.create');
+
+btn.addEventListener('click', ()=> {
+    createSquares();
+})
+
+// container.addEventListener('mouseover', ()=>{
+    
+//     console.log("a7a");
+// })
