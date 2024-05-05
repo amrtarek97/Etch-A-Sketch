7ecd28604132e1390f0337188879903f48b113f5
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-05-05 15:41:10 +0300

    change project skeleton

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-let container = document.querySelector('.sketch-container');
-// let boxDiv;
+// let container = document.querySelector('.sketch-container');
+// // let boxDiv;
 
-function changeBackgroundColor(){
-    this.style.backgroundColor= "black"
-}
-function createSquares(){
+// function changeBackgroundColor(){
+//     this.style.backgroundColor= "black"
+// }
+// function createSquares(){
     
-    for (let i = 0; i < 256; i++) {
-        let boxDiv = document.createElement('div');
-        boxDiv.classList.add("box");
-        container.appendChild(boxDiv);
+//     for (let i = 0; i < 256; i++) {
+//         let boxDiv = document.createElement('div');
+//         boxDiv.classList.add("box");
+//         container.appendChild(boxDiv);
 
-        // boxDiv.addEventListener('mouseover', changeBackgroundColor())
-    }
+//         // boxDiv.addEventListener('mouseover', changeBackgroundColor())
+//     }
     
-}
+// }
 
-let btn = document.querySelector('.create');
+// let btn = document.querySelector('.create');
 
-btn.addEventListener('click', ()=> {
-    createSquares();
-})
+// btn.addEventListener('click', ()=> {
+//     createSquares();
+// })
 
 // container.addEventListener('mouseover', ()=>{
     
 //     console.log("a7a");
 // })
+
+
+// sth else:
+
+const GRIDSIDE = 600;
+let rows = 16;
+let cols = 16;
+
+const sketchArea = document.querySelector("#sketch-area");
+sketchArea.style.width = `${GRIDSIDE}px`;
+sketchArea.style.height = `${GRIDSIDE}px`;
+
