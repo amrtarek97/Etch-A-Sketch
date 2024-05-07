6cc42666813793b475030240d099814135428f3f
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-05-07 12:46:51 +0300

    update javascript

--- script.js
+++ script.js
@@ -38,3 +38,16 @@ const sketchArea = document.querySelector("#sketch-area");
 sketchArea.style.width = `${GRIDSIDE}px`;
 sketchArea.style.height = `${GRIDSIDE}px`;
 
+function createGridCells() {
+    for(let i =0; i< (rows*cols);i++){
+        const gridCell = document.createElement("div");
+
+        gridCell.style.width = `${(GRIDSIDE / cols)-2}px`;
+        gridCell.style.height = `${(GRIDSIDE / rows)-2}px`;
+        gridCell.classList.add("cell");
+        
+        sketchArea.appendChild(gridCell);
+    }
+}
+
+createGridCells();
